93969982dda8b5a5437b205671d78abc2f6c06d0
K Bhavya <kbhavya1976@gmail.com>
2019-08-09 21:55:49 +0530

    Update TrackRepository.java

--- src/main/java/com/stackroute/springboot/muzix/repository/TrackRepository.java
+++ src/main/java/com/stackroute/springboot/muzix/repository/TrackRepository.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface TrackRepository extends JpaRepository<Track, Integer> {
-    @Query(value = "select * from Track where trackName=?1",nativeQuery = true)
-    List<Track> findTitleByName(String name);
+   
 }
